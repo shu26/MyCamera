e28d1a93ebf885b6fb3dc5d71e70caa76bcdc8ee
海法修平 <fnmmbestsk15kkpo@gmail.com>
2019-07-13 13:04:43 +0900

    Initial Commit

---
+++ MyCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCamera
+//
+//  Created by 海法修平 on 2019/07/13.
+//  Copyright © 2019 海法修平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
