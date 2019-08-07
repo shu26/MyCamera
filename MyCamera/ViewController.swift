29cb7ddb41a62d0734db00eff790298600fc74de
海法修平 <fnmmbestsk15kkpo@gmail.com>
2019-08-07 17:05:36 +0900

    [Update] Change UI of first view

--- MyCamera/ViewController.swift
+++ MyCamera/ViewController.swift
@@ -15,50 +15,25 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         // Do any additional setup after loading the view.
     }
 
-    @IBOutlet weak var pictureImage: UIImageView!
+    @IBOutlet weak var pictureImageView: UIImageView!
     
     @IBAction func cameraButton(_ sender: Any) {
-        let alertController = UIAlertController(title: "確認", message: "選択してください", preferredStyle: .actionSheet)
-        
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            let cameraAction = UIAlertAction(title: "カメラ", style: .default, handler: {(action: UIAlertAction) in
-                let imagePickerController = UIImagePickerController()
-                imagePickerController.sourceType = .camera
-                imagePickerController.delegate = self
-                self.present(imagePickerController, animated: true, completion: nil)
-            })
-            alertController.addAction(cameraAction)
-        }
-        
-        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
-            let photoLibraryAction = UIAlertAction(title: "フォトライブラリー", style: .default, handler: {(action: UIAlertAction) in
-                let imagePickerController = UIImagePickerController()
-                imagePickerController.sourceType = .photoLibrary
-                imagePickerController.delegate = self
-                self.present(imagePickerController, animated: true, completion: nil)
-            })
-            alertController.addAction(photoLibraryAction)
-        }
-        let cancelAction = UIAlertAction(title: "キャンセル", style: .cancel, handler: nil)
-        alertController.addAction(cancelAction)
-        
-        // iPad
-//        alertController.popoverPresentationController?.sourceView = view
-        present(alertController, animated: true, completion: nil)
-        
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            print("カメラは利用できます")
-            let imagePickerController = UIImagePickerController()
-            imagePickerController.sourceType = .camera
-            imagePickerController.delegate = self
-            present(imagePickerController, animated: true, completion: nil)
-        } else {
-            print("カメラは利用できません")
-        }
+
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .camera
+        imagePickerController.delegate = self
+        self.present(imagePickerController, animated: true, completion: nil)
     }
     
+    @IBAction func PhotoLibraryButton(_ sender: Any) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        self.present(imagePickerController, animated: true, completion: nil)
+        
+    }
     @IBAction func SNSButton(_ sender: Any) {
-        if let shareImage = pictureImage.image {
+        if let shareImage = pictureImageView.image {
             let shareItems = [shareImage]
             let controller = UIActivityViewController(activityItems: shareItems, applicationActivities: nil)
             // iPadで落ちてしまう対策
@@ -67,8 +42,10 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         }
     }
     
+    // 撮影後
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         captureImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+//        UIImageWriteToSavedPhotosAlbum(captureImage!, nil, nil, nil)
         dismiss(animated: true, completion: {
             self.performSegue(withIdentifier: "showEffectView", sender: nil)
         })
